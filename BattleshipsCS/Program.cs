c163ab546dcb8e760518f2ead963df8ff97bbe36
Richard Pawson <rpawson@nakedobjects.net>
2019-09-24 19:06:20 +0100

    Working version of CS & VB Battleships with Training.txt file each

---
+++ BattleshipsCS/Program.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public struct ShipType
+    {
+        public string Name;
+        public int Size;
+    }
+
+    const string TrainingGame = "Training.txt";
+
+    private static void GetRowColumn(ref int Row, ref int Column)
+    {
+        Console.WriteLine();
+        Console.Write("Please enter column: ");
+        Column = GetRowOrColumnNumber();
+        Console.Write("Please enter row: ");
+        Row = GetRowOrColumnNumber();
+    }
+
+    private static int GetRowOrColumnNumber()
+    {
+        while (true)
+        {
+            var num = Convert.ToInt32(Console.ReadLine());
+            if (num > 9 || num < 0)
+            {
+                Console.Write("Invalid value entered");
+            }
+            else
+            {
+                return num;
+            }
+        }
+    }
+
+    private static void MakePlayerMove(char[,] Board, ShipType[] Ships)
+    {
+        int Row = 0;
+        int Column = 0;
+        GetRowColumn(ref Row, ref Column);
+        if (Board[Row, Column] == 'm' || Board[Row, Column] == 'h')
+        {
+            Console.WriteLine("Sorry, you have already shot at the square (" + Column + "," + Row + "). Please try again.");
+        }
+        else if (Board[Row, Column] == '-')
+        {
+            Console.WriteLine("Sorry, (" + Column + "," + Row + ") is a miss.");
+            Board[Row, Column] = 'm';
+        }
+        else
+        {
+            Console.WriteLine("Hit at (" + Column + "," + Row + ").");
+            Board[Row, Column] = 'h';
+        }
+    }
+
+    private static void SetUpBoard(char[,] Board)
+    {
+        for (int Row = 0; Row < 10; Row++)
+        {
+            for (int Column = 0; Column < 10; Column++)
+            {
+                Board[Row, Column] = '-';
+            }
+        }
+    }
+
+    private static void LoadGame(string TrainingGame, char[,] Board)
+    {
+        string Line = "";
+        var BoardFile = OpenFile(TrainingGame);
+        for (int Row = 0; Row < 10; Row++)
+        {
+            Line = BoardFile.ReadLine();
+            for (int Column = 0; Column < 10; Column++)
+            {
+                Board[Row, Column] = Line[Column];
+            }
+        }
+        BoardFile.Close();
+    }
+
+    private static void PlaceRandomShips(char[,] Board, ShipType[] Ships)
+    {
+        Random RandomNumber = new Random();
+        bool Valid;
+        char Orientation = ' ';
+        int Row = 0;
+        int Column = 0;
+        int HorV = 0;
+        foreach (var Ship in Ships)
+        {
+            Valid = false;
+            while (Valid == false)
+            {
+                Row = RandomNumber.Next(0, 10);
+                Column = RandomNumber.Next(0, 10);
+                HorV = RandomNumber.Next(0, 2);
+                if (HorV == 0)
+                {
+                    Orientation = 'v';
+                }
+                else
+                {
+                    Orientation = 'h';
+                }
+                Valid = ValidateBoatPosition(Board, Ship, Row, Column, Orientation);
+            }
+            Console.WriteLine("Computer placing the " + Ship.Name);
+            PlaceShip(Board, Ship, Row, Column, Orientation);
+        }
+    }
+
+    private static void PlaceShip(char[,] Board, ShipType Ship, int Row, int Column, char Orientation)
+    {
+        if (Orientation == 'v')
+        {
+            for (int Scan = 0; Scan < Ship.Size; Scan++)
+            {
+                Board[Row + Scan, Column] = Ship.Name[0];
+            }
+        }
+        else if (Orientation == 'h')
+        {
+            for (int Scan = 0; Scan < Ship.Size; Scan++)
+            {
+                Board[Row, Column + Scan] = Ship.Name[0];
+            }
+        }
+    }
+
+    private static bool ValidateBoatPosition(char[,] Board, ShipType Ship, int Row, int Column, char Orientation)
+    {
+        if (!WouldFitWithinBoard(Ship, Row, Column, Orientation))
+        {
+            return false;
+        }
+        else
+        {
+            if (Orientation == 'v')
+            {
+                for (int Scan = 0; Scan < Ship.Size; Scan++)
+                {
+                    if (Board[Row + Scan, Column] != '-')
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (Orientation == 'h')
+            {
+                for (int Scan = 0; Scan < Ship.Size; Scan++)
+                {
+                    if (Board[Row, Column + Scan] != '-')
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+    private static bool WouldFitWithinBoard(ShipType Ship, int Row, int Column, char Orientation)
+    {
+        if (Orientation == 'v' && Row + Ship.Size > 10)
+        {
+            return false;
+        }
+        else if (Orientation == 'h' && Column + Ship.Size > 10)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private static bool CheckWin(char[,] Board)
+    {
+        for (int Row = 0; Row < 10; Row++)
+        {
+            for (int Column = 0; Column < 10; Column++)
+            {
+                if (Board[Row, Column] == 'A' || Board[Row, Column] == 'B' || Board[Row, Column] == 'S' || Board[Row, Column] == 'D' || Board[Row, Column] == 'P')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void PrintBoard(char[,] Board)
+    {
+        Console.WriteLine();
+        Console.WriteLine("The board looks like this: ");
+        Console.WriteLine();
+        Console.Write(" ");
+        for (int Column = 0; Column < 10; Column++)
+        {
+            Console.Write(" " + Column + "  ");
+        }
+        Console.WriteLine();
+        for (int Row = 0; Row < 10; Row++)
+        {
+            Console.Write(Row + " ");
+            for (int Column = 0; Column < 10; Column++)
+            {
+                if (Board[Row, Column] == '-')
+                {
+                    Console.Write(" ");
+                }
+                else if (Board[Row, Column] == 'A' || Board[Row, Column] == 'B' || Board[Row, Column] == 'S' || Board[Row, Column] == 'D' || Board[Row, Column] == 'P')
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write(Board[Row, Column]);
+                }
+                if (Column != 9)
+                {
+                    Console.Write(" | ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("MAIN MENU");
+        Console.WriteLine("");
+        Console.WriteLine("1. Start new game");
+        Console.WriteLine("2. Load training game");
+        Console.WriteLine("9. Quit");
+        Console.WriteLine();
+    }
+
+    private static int GetMainMenuChoice()
+    {
+        int Choice = 0;
+        Console.Write("Please enter your choice: ");
+        Choice = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        return Choice;
+    }
+
+    private static void PlayGame(char[,] Board, ShipType[] Ships)
+    {
+        bool GameWon = false;
+        while (GameWon == false)
+        {
+            PrintBoard(Board);
+            MakePlayerMove(Board, Ships);
+            GameWon = CheckWin(Board);
+            if (GameWon == true)
+            {
+                Console.WriteLine("All ships sunk!");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void SetUpShips(ShipType[] Ships)
+    {
+        Ships[0].Name = "Aircraft Carrier";
+        Ships[0].Size = 5;
+        Ships[1].Name = "Battleship";
+        Ships[1].Size = 4;
+        Ships[2].Name = "Submarine";
+        Ships[2].Size = 3;
+        Ships[3].Name = "Destroyer";
+        Ships[3].Size = 3;
+        Ships[4].Name = "Patrol Boat";
+        Ships[4].Size = 2;
+    }
+
+    private static StreamReader OpenFile(string fileName, string path = "")
+    {
+        return new StreamReader(path + fileName);
+    }
+
+    static void Main()
+    {
+        ShipType[] Ships = new ShipType[5];
+        char[,] Board = new char[10, 10];
+        int MenuOption = 0;
+        while (MenuOption != 9)
+        {
+            SetUpBoard(Board);
+            SetUpShips(Ships);
+            DisplayMenu();
+            MenuOption = GetMainMenuChoice();
+            if (MenuOption == 1)
+            {
+                PlaceRandomShips(Board, Ships);
+                PlayGame(Board, Ships);
+            }
+            if (MenuOption == 2)
+            {
+                LoadGame(TrainingGame, Board);
+                PlayGame(Board, Ships);
+            }
+        }
+    }
+}
---
+++ BattleshipsVB/Program.vb
@@ -0,0 +1,268 @@
+Imports System.IO
+
+Module Program
+    Public Structure ShipType
+        Public Name As String
+        Public Size As Integer
+    End Structure
+
+    Const TrainingGame As String = "Training.txt"
+
+    Sub GetRowColumn(ByRef Row As Integer, ByRef Column As Integer)
+        Console.WriteLine()
+        Console.Write("Please enter column: ")
+        Column = Convert.ToInt32(Console.ReadLine())
+        Dim isValidRow As Boolean = False
+
+        While isValidRow
+            Row = Convert.ToInt32(Console.ReadLine())
+
+            If Row > 9 OrElse Row < 0 Then
+                Console.Write("Invalid value entered")
+            Else
+                isValidRow = True
+            End If
+        End While
+    End Sub
+
+    Sub MakePlayerMove(ByVal Board As Char(,), ByVal Ships As ShipType())
+        Dim Row As Integer = 0
+        Dim Column As Integer = 0
+        GetRowColumn(Row, Column)
+
+        If Board(Row, Column) = "m"c OrElse Board(Row, Column) = "h"c Then
+            Console.WriteLine("Sorry, you have already shot at the square (" & Column & "," & Row & "). Please try again.")
+        ElseIf Board(Row, Column) = "-"c Then
+            Console.WriteLine("Sorry, (" & Column & "," & Row & ") is a miss.")
+            Board(Row, Column) = "m"c
+        Else
+            Console.WriteLine("Hit at (" & Column & "," & Row & ").")
+            Board(Row, Column) = "h"c
+        End If
+    End Sub
+
+    Sub SetUpBoard(ByVal Board As Char(,))
+        For Row As Integer = 0 To 10 - 1
+
+            For Column As Integer = 0 To 10 - 1
+                Board(Row, Column) = "-"c
+            Next
+        Next
+    End Sub
+
+    Sub LoadGame(ByVal TrainingGame As String, ByVal Board As Char(,))
+        Dim Line As String = ""
+        Dim BoardFile = OpenFile(TrainingGame)
+
+        For Row As Integer = 0 To 10 - 1
+            Line = BoardFile.ReadLine()
+
+            For Column As Integer = 0 To 10 - 1
+                Board(Row, Column) = Line(Column)
+            Next
+        Next
+
+        BoardFile.Close()
+    End Sub
+
+    Sub PlaceRandomShips(ByVal Board As Char(,), ByVal Ships As ShipType())
+        Dim RandomNumber As Random = New Random()
+        Dim Valid As Boolean
+        Dim Orientation As Char = " "c
+        Dim Row As Integer = 0
+        Dim Column As Integer = 0
+        Dim HorV As Integer = 0
+
+        For Each Ship In Ships
+            Valid = False
+
+            While Valid = False
+                Row = RandomNumber.[Next](0, 10)
+                Column = RandomNumber.[Next](0, 10)
+                HorV = RandomNumber.[Next](0, 2)
+
+                If HorV = 0 Then
+                    Orientation = "v"c
+                Else
+                    Orientation = "h"c
+                End If
+
+                Valid = ValidateBoatPosition(Board, Ship, Row, Column, Orientation)
+            End While
+
+            Console.WriteLine("Computer placing the " & Ship.Name)
+            PlaceShip(Board, Ship, Row, Column, Orientation)
+        Next
+    End Sub
+
+    Sub PlaceShip(ByVal Board As Char(,), ByVal Ship As ShipType, ByVal Row As Integer, ByVal Column As Integer, ByVal Orientation As Char)
+        If Orientation = "v"c Then
+
+            For Scan As Integer = 0 To Ship.Size - 1
+                Board(Row + Scan, Column) = Ship.Name(0)
+            Next
+        ElseIf Orientation = "h"c Then
+
+            For Scan As Integer = 0 To Ship.Size - 1
+                Board(Row, Column + Scan) = Ship.Name(0)
+            Next
+        End If
+    End Sub
+
+    Function ValidateBoatPosition(ByVal Board As Char(,), ByVal Ship As ShipType, ByVal Row As Integer, ByVal Column As Integer, ByVal Orientation As Char) As Boolean
+        If Not WouldFitWithinBoard(Ship, Row, Column, Orientation) Then
+            Return False
+        Else
+
+            If Orientation = "v"c Then
+
+                For Scan As Integer = 0 To Ship.Size - 1
+
+                    If Board(Row + Scan, Column) <> "-"c Then
+                        Return False
+                    End If
+                Next
+            ElseIf Orientation = "h"c Then
+
+                For Scan As Integer = 0 To Ship.Size - 1
+
+                    If Board(Row, Column + Scan) <> "-"c Then
+                        Return False
+                    End If
+                Next
+            End If
+        End If
+
+        Return True
+    End Function
+
+    Function WouldFitWithinBoard(ByVal Ship As ShipType, ByVal Row As Integer, ByVal Column As Integer, ByVal Orientation As Char) As Boolean
+        If Orientation = "v"c AndAlso Row + Ship.Size > 10 Then
+            Return False
+        ElseIf Orientation = "h"c AndAlso Column + Ship.Size > 10 Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Function CheckWin(ByVal Board As Char(,)) As Boolean
+        For Row As Integer = 0 To 10 - 1
+
+            For Column As Integer = 0 To 10 - 1
+
+                If Board(Row, Column) = "A"c OrElse Board(Row, Column) = "B"c OrElse Board(Row, Column) = "S"c OrElse Board(Row, Column) = "D"c OrElse Board(Row, Column) = "P"c Then
+                    Return False
+                End If
+            Next
+        Next
+
+        Return True
+    End Function
+
+    Sub PrintBoard(ByVal Board As Char(,))
+        Console.WriteLine()
+        Console.WriteLine("The board looks like this: ")
+        Console.WriteLine()
+        Console.Write(" ")
+
+        For Column As Integer = 0 To 10 - 1
+            Console.Write(" " & Column & "  ")
+        Next
+
+        Console.WriteLine()
+
+        For Row As Integer = 0 To 10 - 1
+            Console.Write(Row & " ")
+
+            For Column As Integer = 0 To 10 - 1
+
+                If Board(Row, Column) = "-"c Then
+                    Console.Write(" ")
+                ElseIf Board(Row, Column) = "A"c OrElse Board(Row, Column) = "B"c OrElse Board(Row, Column) = "S"c OrElse Board(Row, Column) = "D"c OrElse Board(Row, Column) = "P"c Then
+                    Console.Write(" ")
+                Else
+                    Console.Write(Board(Row, Column))
+                End If
+
+                If Column <> 9 Then
+                    Console.Write(" | ")
+                End If
+            Next
+
+            Console.WriteLine()
+        Next
+    End Sub
+
+    Sub DisplayMenu()
+        Console.WriteLine("MAIN MENU")
+        Console.WriteLine("")
+        Console.WriteLine("1. Start new game")
+        Console.WriteLine("2. Load training game")
+        Console.WriteLine("9. Quit")
+        Console.WriteLine()
+    End Sub
+
+    Function GetMainMenuChoice() As Integer
+        Dim Choice As Integer = 0
+        Console.Write("Please enter your choice: ")
+        Choice = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine()
+        Return Choice
+    End Function
+
+    Sub PlayGame(ByVal Board As Char(,), ByVal Ships As ShipType())
+        Dim GameWon As Boolean = False
+
+        While GameWon = False
+            PrintBoard(Board)
+            MakePlayerMove(Board, Ships)
+            GameWon = CheckWin(Board)
+
+            If GameWon = True Then
+                Console.WriteLine("All ships sunk!")
+                Console.WriteLine()
+            End If
+        End While
+    End Sub
+
+    Sub SetUpShips(ByVal Ships As ShipType())
+        Ships(0).Name = "Aircraft Carrier"
+        Ships(0).Size = 5
+        Ships(1).Name = "Battleship"
+        Ships(1).Size = 4
+        Ships(2).Name = "Submarine"
+        Ships(2).Size = 3
+        Ships(3).Name = "Destroyer"
+        Ships(3).Size = 3
+        Ships(4).Name = "Patrol Boat"
+        Ships(4).Size = 2
+    End Sub
+
+    Function OpenFile(ByVal fileName As String, ByVal Optional path As String = "") As StreamReader
+        Return New StreamReader(path & fileName)
+    End Function
+
+    Sub Main()
+        Dim Ships As ShipType() = New ShipType(4) {}
+        Dim Board As Char(,) = New Char(9, 9) {}
+        Dim MenuOption As Integer = 0
+
+        While MenuOption <> 9
+            SetUpBoard(Board)
+            SetUpShips(Ships)
+            DisplayMenu()
+            MenuOption = GetMainMenuChoice()
+
+            If MenuOption = 1 Then
+                PlaceRandomShips(Board, Ships)
+                PlayGame(Board, Ships)
+            End If
+
+            If MenuOption = 2 Then
+                LoadGame(TrainingGame, Board)
+                PlayGame(Board, Ships)
+            End If
+        End While
+    End Sub
+End Module
